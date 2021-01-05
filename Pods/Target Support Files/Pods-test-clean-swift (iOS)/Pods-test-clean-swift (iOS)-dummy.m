cb19e4e62c5dbd640b5253e77e3baecac8498e34
toy <>
2021-01-05 13:22:22 +0700

    add gitignore

---
+++ Pods/Target Support Files/Pods-test-clean-swift (iOS)/Pods-test-clean-swift (iOS)-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_test_clean_swift__iOS_ : NSObject
+@end
+@implementation PodsDummy_Pods_test_clean_swift__iOS_
+@end
---
+++ Pods/Target Support Files/Pods-test-clean-swift (macOS)/Pods-test-clean-swift (macOS)-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_test_clean_swift__macOS_ : NSObject
+@end
+@implementation PodsDummy_Pods_test_clean_swift__macOS_
+@end
